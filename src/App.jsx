e9e047e05c710bccd880f427db235e2c4090e324
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-12 13:17:19 +0530

    routing applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
 
 function App() {
   return (
-    <div>App</div>
+    <div className="w-screen h-screen">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   )
 }
 
