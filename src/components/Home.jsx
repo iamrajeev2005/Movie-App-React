6668a13bdf6f46820d3a46b303d32bd1b3820c5b
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-13 14:15:21 +0530

    searchbar making

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Sidebar from "./partials/Sidebar";
+import Searchbar from "./partials/Searchbar";
 
 function Home() {
   document.title = "Homepage";
   return (
     <>
       <Sidebar />
-      <div className="w-[80%] h-full "></div>
+      <div className="w-[80%] h-full ">
+        <Searchbar/>
+      </div>
     </>
   );
 }
