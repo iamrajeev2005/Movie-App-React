0bda5de89ce9e79f5fa97ef93b4ce10e8fdb3495
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-16 22:14:29 +0530

    home scroll fix

--- src/components/partials/Cards.jsx
+++ src/components/partials/Cards.jsx
@@ -15,20 +15,22 @@ function Cards() {
     getCardImg();
   },[category]);
   return (
-    <div className="horizontal-scroll overflow-x-auto">
-      <div className=" px-4 w-full h-fit my-2">
-        <div className="flex items-center justify-between">
-          <h1 className="text-white font-semibold text-2xl py-2">Trending</h1>
-          <Dropdown
-            title="Filter"
-            options={["tv", "movie", "all"]}
-            filter={(e) => setcategory(e.target.value)}
-          />
-        </div>
-        <div className="flex w-[315vw] items-center gap-4">
-          {cardImg.map((item, index) => {
-            return <Card key={index} item={item} />;
-          })}
+    <div>
+      <div className="flex items-center justify-between px-4">
+        <h1 className="text-white font-semibold text-2xl py-2 ">Trending</h1>
+        <Dropdown
+          title="Filter"
+          options={["tv", "movie", "all"]}
+          filter={(e) => setcategory(e.target.value)}
+        />
+      </div>
+      <div className="horizontal-scroll overflow-x-auto">
+        <div className=" px-4 w-full h-fit my-2">
+          <div className="flex w-[315vw] items-center gap-4">
+            {cardImg.map((item, index) => {
+              return <Card key={index} item={item} />;
+            })}
+          </div>
         </div>
       </div>
     </div>
