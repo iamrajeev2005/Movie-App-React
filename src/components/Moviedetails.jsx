014a4e12c1c8f801d5c995c6f86cc69d69ae997c
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-23 11:59:04 +0530

    added overview

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -23,22 +23,27 @@ function Moviedetails() {
         alt=""
       />
       <div className="flex items-start absolute top-0 left-0">
-        <div className="h-[20vh] w-[20vw] object-cover mt-20 ml-20">
+        <div className=" w-[40vw] object-cover mt-20 ml-20">
           <img
             src={`https://image.tmdb.org/t/p/original/${info.detail.poster_path}`}
             alt=""
           />
         </div>
-        <div className="mt-20 ml-20 flex items-end">
-          <h1 className="text-5xl font-bold text-white">
-            {info.detail.name ||
-              info.detail.original_title ||
-              info.detail.title ||
-              info.detail.original_name}
-          </h1>
-          <h3 className="text-xl font-bold text-zinc-200">
-            ({info.detail.release_date})
-          </h3>
+        <div className="mt-20 ml-20 flex flex-col items-start">
+          <div className=" flex items-end justify-center">
+            <h1 className="text-5xl font-bold text-white">
+              {info.detail.name ||
+                info.detail.original_title ||
+                info.detail.title ||
+                info.detail.original_name}
+            </h1>
+            <h3 className="text-xl font-bold text-zinc-200">
+              ({info.detail.release_date})
+            </h3>
+          </div>
+          <p className="text-md font-semibold mt-6 text-white w-[70%]">
+            {info.detail.overview}
+          </p>
         </div>
       </div>
     </div>
