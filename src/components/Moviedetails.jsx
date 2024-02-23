b0208cf240e90cf9077fe6f5017e047b80199bc0
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-23 11:20:48 +0530

    added poster path and name in moviedetail

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { asyncSetMovies, resetInfo } from "../store/actions/movieAction";
 import { useParams } from "react-router-dom";
+import Searchbar from "../components/partials/Searchbar";
 
 function Moviedetails() {
   const { info } = useSelector((state) => state.movie);
@@ -17,15 +18,30 @@ function Moviedetails() {
   return info ? (
     <div
       style={{
-        background:
-          `url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
-          backgroundPosition:`center`,
-          backgroundSize:`cover`,
-          backgroundRepeat:`no-repeat`,
-          opacity:`0.3`
+        background: `url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
+        backgroundPosition: `center`,
+        backgroundSize: `cover`,
+        backgroundRepeat: `no-repeat`,
       }}
-      className="w-screen h-screen "
-    ></div>
+      className="w-screen h-screen relative"
+    >
+      <div className="flex items-start ">
+        <div className="h-[20vh] w-[20vw] object-cover mt-20 ml-20">
+          <img
+            src={`https://image.tmdb.org/t/p/original/${info.detail.poster_path}`}
+            alt=""
+          />
+        </div>
+        <div className="mt-20 ml-20">
+          <h1 className="text-5xl font-bold text-white">
+            {info.detail.name ||
+              info.detail.original_title ||
+              info.detail.title ||
+              info.detail.original_name}
+          </h1>
+        </div>
+      </div>
+    </div>
   ) : (
     <h1>Loading</h1>
   );
