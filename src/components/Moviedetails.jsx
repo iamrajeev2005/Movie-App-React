5283cf9a7b1e8a9acd0ef4ffe22e3b93af2e8169
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-23 11:45:25 +0530

    opacity changed

--- src/components/Moviedetails.jsx
+++ src/components/Moviedetails.jsx
@@ -16,29 +16,29 @@ function Moviedetails() {
     };
   }, []);
   return info ? (
-    <div
-      style={{
-        background: `url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
-        backgroundPosition: `center`,
-        backgroundSize: `cover`,
-        backgroundRepeat: `no-repeat`,
-      }}
-      className="w-screen h-screen relative"
-    >
-      <div className="flex items-start ">
+    <div className="w-screen h-screen relative">
+      <img
+        className="h-screen w-screen opacity-30 object-cover object-center"
+        src={`https://image.tmdb.org/t/p/original/${info.detail.backdrop_path}`}
+        alt=""
+      />
+      <div className="flex items-start absolute top-0 left-0">
         <div className="h-[20vh] w-[20vw] object-cover mt-20 ml-20">
           <img
             src={`https://image.tmdb.org/t/p/original/${info.detail.poster_path}`}
             alt=""
           />
         </div>
-        <div className="mt-20 ml-20">
+        <div className="mt-20 ml-20 flex items-end">
           <h1 className="text-5xl font-bold text-white">
             {info.detail.name ||
               info.detail.original_title ||
               info.detail.title ||
               info.detail.original_name}
           </h1>
+          <h3 className="text-xl font-bold text-zinc-200">
+            ({info.detail.release_date})
+          </h3>
         </div>
       </div>
     </div>
