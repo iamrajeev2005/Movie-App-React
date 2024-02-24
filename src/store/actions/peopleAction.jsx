22b09f4cc2102b4bf053edcd8e576fe1f5dd99f3
iamrajeev2005 <fea.rajeev@gmail.com>
2024-02-24 23:01:06 +0530

    people acton started

---
+++ src/store/actions/peopleAction.jsx
@@ -0,0 +1,16 @@
+export { resetInfo } from "../reducers/peopleSlice";
+import axios from "../../../utils/axios";
+import { setInfo } from "../reducers/peopleSlice";
+
+export const asyncSetperson = (id) => async (dispatch, getState) => {
+  try {
+    const detail = await axios.get(`/person/${id}`);
+    let allDetails = {
+      detail: detail.data,
+    };
+    dispatch(setInfo(allDetails));
+    console.log(allDetails);
+  } catch (error) {
+    console.log(error);
+  }
+};
